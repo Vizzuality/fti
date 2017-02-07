a753823c93bb0f4aa3f369ad3af92a87e44fb5d3
rschumann <sebastian.schkudlara@gmail.com>
2017-02-07 15:56:16 +0100

    Remove should from specs

--- spec/models/ability_spec.rb
+++ spec/models/ability_spec.rb
@@ -7,23 +7,23 @@ RSpec.describe Ability, type: :model do
     @admin = create(:admin)
   end
 
-  it { Ability.should include(CanCan::Ability)          }
-  it { Ability.should respond_to(:new).with(1).argument }
+  it { expect(Ability).to include(CanCan::Ability)          }
+  it { expect(Ability).to respond_to(:new).with(1).argument }
 
   describe 'Ability' do
     context 'When is a user' do
       it 'Can manage owned profile' do
-        Ability.any_instance.should_receive(:can).with([:read], :all)
-        Ability.any_instance.should_receive(:can).with([:manage], User, id: @user.id)
-        Ability.any_instance.should_receive(:can).with([:manage], UserPermission, user_id: @user.id)
+        expect_any_instance_of(Ability).to receive(:can).with([:read], :all)
+        expect_any_instance_of(Ability).to receive(:can).with([:manage], User, id: @user.id)
+        expect_any_instance_of(Ability).to receive(:can).with([:manage], UserPermission, user_id: @user.id)
         Ability.new @user
       end
     end
 
     context 'When is an admin' do
       it 'Can manage all' do
-        Ability.any_instance.should_receive(:can).with([:read], :admin)
-        Ability.any_instance.should_receive(:can).with([:manage], :all)
+        expect_any_instance_of(Ability).to receive(:can).with([:read], :admin)
+        expect_any_instance_of(Ability).to receive(:can).with([:manage], :all)
         Ability.new @admin
       end
     end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -67,6 +67,6 @@ RSpec.configure do |config|
   end
 
   config.include FactoryGirl::Syntax::Methods
-  config.include Devise::TestHelpers, type: :controller
+  config.include Devise::Test::ControllerHelpers, type: :controller
   config.order = "random"
 end
