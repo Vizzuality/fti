b58bbcf46d3d1486af4e766be3d141541569e169
rschumann <sebastian.schkudlara@gmail.com>
2017-02-09 10:25:55 +0100

    Fixes for store location

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,9 +2,9 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :menu_highlight
-  after_action  :store_location
   before_action :set_locale, unless: -> { request.xhr? }
   before_action :get_locale
+  after_action  :store_location
 
   def default_url_options(options={})
     { locale: I18n.locale }
@@ -26,15 +26,15 @@ class ApplicationController < ActionController::Base
 
     def store_location
       return unless request.get?
-      if request.path != root_path &&
-        request.path != '/account/login' &&
-        request.path != '/account/register' &&
-        request.path != '/account/secret/new' &&
-        request.path != '/account/secret/edit' &&
-        request.path != '/account/logout' &&
-        request.path != '/account/edit' &&
-        request.path != '/account/password' &&
-        request.path != '/account/cancel' &&
+      if request.fullpath.match(root_path) &&
+        request.fullpath.match('/account/login') &&
+        request.fullpath.match('/account/register') &&
+        request.fullpath.match('/account/secret/new') &&
+        request.fullpath.match('/account/secret/edit') &&
+        request.fullpath.match('/account/logout') &&
+        request.fullpath.match('/account/edit') &&
+        request.fullpath.match('/account/password') &&
+        request.fullpath.match('/account/cancel') &&
         !request.xhr? # don't store ajax calls
         session[:previous_url] = request.fullpath
       end
